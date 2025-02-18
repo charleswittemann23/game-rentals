27f918eae56bf4784ab958ff56acfeb956f97445
Dawson-Codes <dawsontj@comcast.net>
2025-02-18 12:57:45 -0500

    Controller and view checking to see who is logged in.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    return render(request, 'home/index.html')
+    if request.user.is_authenticated:
+        if request.user.groups.filter(name='Librarians').exists():
+            role = 'librarian'
+        else:
+            role = 'user'
+    else:
+        role = None
+
+    return render(request, 'home/index.html', {'role': role})
