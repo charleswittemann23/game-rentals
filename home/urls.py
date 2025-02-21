ff734b8208c8c5c4117444442e3da59cb34895fb
Jonah Lee <jonahsjlee03@gmail.com>
2025-02-20 19:34:31 -0500

    2/20:two different pages show based on whether user is admin or not

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+from .views import dashboard
 
 app_name = "home"
 urlpatterns = [
     path("", views.index, name="index"),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def index(request):
-    if request.user.is_authenticated:
-        if request.user.groups.filter(name='Librarians').exists():
-            role = 'librarian'
-        else:
-            role = 'user'
+    if request.user.is_superuser:  # Check if user is an admin
+        return render(request, "home/admin.html")
     else:
-        role = None
+        return render(request, "home/index.html")
+
+    # if request.user.is_authenticated:
+    #     if request.user.groups.filter(name='Librarians').exists():
+    #         role = 'librarian'
+    #     else:
+    #         role = 'user'
+    # else:
+    #     role = None
 
     return render(request, 'home/index.html', {'role': role})
+
+@login_required
+def dashboard(request):
+    return render(request, "home/dashboard.html")
