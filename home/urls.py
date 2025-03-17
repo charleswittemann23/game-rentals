8c07a1aa8e70b49ae75c5880c70999345c822876
Dawson-Codes <dawsontj@comcast.net>
2025-03-17 15:53:38 -0400

    Wrong urls

--- home/urls.py
+++ home/urls.py
@@ -4,10 +4,9 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = "home"
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
-    path("catalog/", include("catalog.urls")),
-
-]
+    path("", views.index, name="index"),
+    path("wishlist/", views.wishlist, name="wishlist"),
+    path("update_user/", views.update_user,name='update_user')
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
