e9ff2cc5642ca2eabae38f088feaf23051d1f57e
Dawson-Codes <dawsontj@comcast.net>
2025-03-17 15:52:49 -0400

    Update requirements, refactor urls

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,10 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = "home"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("wishlist/", views.wishlist, name="wishlist"),
-    path("update_user/", views.update_user,name='update_user')
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
+    path("catalog/", include("catalog.urls")),
+
+]
