a2632cddd6e27647baf3897ab27c1c7c97f939fd
Dawson-Codes <dawsontj@comcast.net>
2025-04-12 19:05:51 -0400

    Remove account templates and redirect to google login

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', RedirectView.as_view(url='/accounts/google/login/', permanent=False)),
     path('accounts/', include('allauth.urls')),
+
     path('', include('home.urls')),
     path("catalog/", include("catalog.urls")),
     path('collections/', include('collection.urls')),
