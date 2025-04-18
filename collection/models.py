5b14c48a78440b0ce2c7e26ebf549c72f1afd063
Jonah Lee <jonahsjlee03@gmail.com>
2025-04-17 23:58:30 -0400

    made it so that games in private collections cannot belong to other collections

--- collection/models.py
+++ collection/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from catalog.models import Game
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 
 class Collection(models.Model):
@@ -15,6 +16,28 @@ class Collection(models.Model):
 
     def __str__(self):
         return f"{self.name} by {self.creator.username}"
+
+    def can_add_game(self, game):
+        # If the game is already in a private collection, it can't be added to any other collection
+        if game.is_in_private_collection:
+            return False
+        # If this collection is private and the game is in any other collection, it can't be added
+        if self.is_private and game.collections.exists():
+            return False
+        return True
+
+    def add_game(self, game):
+        if not self.can_add_game(game):
+            raise ValidationError("Cannot add game to collection. Game is already in a private collection or this is a private collection and the game is in another collection.")
+        self.games.add(game)
+
+    def save(self, *args, **kwargs):
+        # If this collection is being made private, ensure none of its games are in other collections
+        if self.is_private and self.pk:  # Only check for existing collections
+            for game in self.games.all():
+                if game.collections.exclude(pk=self.pk).exists():
+                    raise ValidationError(f"Cannot make collection private. Game '{game.title}' is already in another collection.")
+        super().save(*args, **kwargs)
 
 class CollectionAccessRequest(models.Model):
     PENDING = 'pending'
