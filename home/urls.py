044947c711c06939a6ce339c53f2951a50eda9de
Charlie Wittemann <charliewittemann@icloud.com>
2025-04-29 14:32:44 -0400

    removal of wishlist path

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,5 @@ from django.conf.urls.static import static
 app_name = "home"
 urlpatterns = [
                   path("", views.index, name="index"),
-                  path("wishlist/", views.wishlist, name="wishlist"),
                   path("update_user/", views.update_user, name='update_user')
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
